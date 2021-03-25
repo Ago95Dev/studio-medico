4b7a3dbd4d20ba7c201ca668e99e50721ac4bed4
NoMeName <francescob29694@gmail.com>
2021-03-25 18:19:00 +0100

    attenzione alla parte utente service e usdb

--- src/main/java/it/univaq/disim/isp/studiomedico/business/impl/UtenteServiceDB.java
+++ src/main/java/it/univaq/disim/isp/studiomedico/business/impl/UtenteServiceDB.java
@@ -109,5 +109,54 @@ public class UtenteServiceDB extends ConnessioneDB implements UtenteService {
         }
         return utente;
     }
-}
 
+
+    public Utente registrazioneMedico(String password, String nome, String cognome, String codicef, String email, String telefono, String data, String luogo, Specializzazione specializzazione) throws BusinessException {
+        Utente utente = null;
+        String query = "insert into utente(password,nome,cognome,codicef,email,telefono,data,luogo,specializzazione)" + "values(?,?,?,?,?,?,?,?,?)";
+        String query2 = "select * from utente where codicef=?";
+
+        try (PreparedStatement st = con.prepareStatement(query)) {
+
+            st.setString(1, password);
+            st.setString(2, nome);
+            st.setString(3, cognome);
+            st.setString(4, codicef);
+            st.setString(5, email);
+            st.setString(6, String.valueOf(data));
+            st.setString(7, String.valueOf(specializzazione));
+            int res = st.executeUpdate();
+
+
+            try (PreparedStatement s1 = con.prepareStatement(query2)) {
+
+                s1.setString(1, codicef);
+
+
+                try (ResultSet rs = s1.executeQuery()) {
+                    while (rs.next()) {
+                        utente = new Medico();
+                        utente.setId(rs.getInt("Id"));
+                        utente.setEmail(email);
+                        utente.setCf(codicef);
+                        utente.setRuolo(Ruolo.paziente);
+                        utente.setNome(nome);
+                        utente.setCognome(cognome);
+                        utente.setPassword(password);
+                        utente.setDataDiNascita(Date.valueOf(data));
+                        utente.setLuogoDiNascita(luogo);
+                        utente.setTelefono(telefono);
+                        ((Medico) utente).setSpecializzazione(specializzazione);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    throw new BusinessException("Errore esecuzione query", e);
+                }
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return utente;
+    }
+}
--- src/main/java/it/univaq/disim/isp/studiomedico/domain/Medico.java
+++ src/main/java/it/univaq/disim/isp/studiomedico/domain/Medico.java
@@ -5,21 +5,62 @@ import java.util.Set;
 
 public class Medico extends Paziente {
 
-	private Specializzazione specializzazione = Specializzazione.CARDIOLOGIA;
+	private Specializzazione specializzazione;
 	private Integer numeropresenze, numerovisite;
 	private Contratto contratto;
 	private Set<Turno> listaTurni;
 
-	public Medico(Integer id, String nome, String cognome, String cf, String telefono, String luogoDiNascita, String email, String password, Date dataDiNascita) {
+	public Medico(Integer id, String nome, String cognome, String cf, String telefono, String luogoDiNascita, String email, String password, Date dataDiNascita, Specializzazione specializzazione) {
 		super(id, nome, cognome, cf, telefono, luogoDiNascita, email, password, dataDiNascita);
+		this.specializzazione=specializzazione;
 	}
-
     public Medico() {
         super();
+        Specializzazione s;
     }
 
     // public Medico(String nome, String cognome, String mail, String password) {
 	//	super(nome, cognome, mail, password);
+
+	public Specializzazione getSpecializzazione() {
+		return specializzazione;
 	}
+
+	public void setSpecializzazione(Specializzazione specializzazione) {
+		this.specializzazione = specializzazione;
+	}
+
+	public Integer getNumeropresenze() {
+		return numeropresenze;
+	}
+
+	public void setNumeropresenze(Integer numeropresenze) {
+		this.numeropresenze = numeropresenze;
+	}
+
+	public Integer getNumerovisite() {
+		return numerovisite;
+	}
+
+	public void setNumerovisite(Integer numerovisite) {
+		this.numerovisite = numerovisite;
+	}
+
+	public Contratto getContratto() {
+		return contratto;
+	}
+
+	public void setContratto(Contratto contratto) {
+		this.contratto = contratto;
+	}
+
+	public Set<Turno> getListaTurni() {
+		return listaTurni;
+	}
+
+	public void setListaTurni(Set<Turno> listaTurni) {
+		this.listaTurni = listaTurni;
+	}
+}
 	
 
